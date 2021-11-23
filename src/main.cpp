cfa4f18eb5d7a4ebf49b4738d24b34b76c0fbad8
Nicolas Phister <nicolas.phister@dont-nod.com>
2021-11-23 16:19:24 +0100

    add character

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,15 @@
 
 #include "go_to_bin_dir.hpp"
 #include "kengine.hpp"
+#include "data/GraphicsComponent.hpp"
+#include "data/TransformComponent.hpp"
 #include "data/WindowComponent.hpp"
 #include "functions/Execute.hpp"
 
 #include "systems/sfml/SFMLSystem.hpp"
 
 #include "helpers/MainLoop.hpp"
+#include "systems/model_creator/ModelCreatorSystem.hpp"
 
 int main(int, char ** av) {
 	putils::goToBinDir(av[0]);
@@ -18,20 +21,26 @@ int main(int, char ** av) {
 #endif
 
 	kengine::init(std::thread::hardware_concurrency());
+	kengine::entities += kengine::ModelCreatorSystem();
 	kengine::entities += kengine::SFMLSystem();
 
-	kengine::entities += [](kengine::Entity & e) {
-		e += kengine::WindowComponent{
+	kengine::entities += [](kengine::Entity & window) {
+		window += kengine::WindowComponent{
 			.name = "Sandbox"
 		};
 
-		e += kengine::functions::Execute{
+		window += kengine::functions::Execute{
 			[](float deltaTime) {
 				if (ImGui::Begin("Zob")) {
 				}
 				ImGui::End();
 			}
 		};
+	};
+
+	kengine::entities += [](kengine::Entity & character) {
+		character += kengine::GraphicsComponent{ .appearance = "resources/textures/character.png" };
+		character += kengine::TransformComponent{};
 	};
 
 	kengine::mainLoop::timeModulated::run();
