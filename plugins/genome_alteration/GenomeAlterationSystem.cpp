d3935932333df1beee20c96fbbb25fa00451ebec
Nicolas Phister <nicolas.phister@dont-nod.com>
2021-12-23 18:54:35 +0100

    use mutationHelper in GenomeAlterationSystem
    + add possibility for multiple mutations in single frame

--- plugins/genome_alteration/GenomeAlterationSystem.cpp
+++ plugins/genome_alteration/GenomeAlterationSystem.cpp
@@ -2,25 +2,25 @@
 #include "helpers/pluginHelper.hpp"
 #include "Export.hpp"
 
+// kengine
 #include "data/AdjustableComponent.hpp"
-
 #include "functions/Execute.hpp"
-
 #include "meta/ForEachEntity.hpp"
-#include "meta/Get.hpp"
-#include "meta/Size.hpp"
-
 #include "helpers/meta/attributeHelper.hpp"
 
+// project
 #include "meta/GenomeComponent.hpp"
+#include "helpers/mutationHelper.hpp"
 
-#include "BitFieldRef.hpp"
+// putils
 #include "rand.hpp"
 
 namespace {
-	static float g_timeSinceLastMutation = 0.f;
-	static float g_timeBetweenMutations = 1.f;
-	static float g_mutationChance = 0.1f;
+	float g_timeSinceLastMutation = 0.f;
+	float g_timeBetweenMutations = 1.f;
+	float g_mutationChance = 0.1f;
+    int g_minMutationCount = 1;
+    int g_maxMutationCount = 10;
 
     struct GenomeAlterationSystem {
         static void init(kengine::Entity & e) noexcept {
@@ -28,7 +28,9 @@ namespace {
             e += kengine::AdjustableComponent{
                 "Genome alteration", {
                     { "Time between mutations", &g_timeBetweenMutations },
-                    { "Mutation chance", &g_mutationChance }
+                    { "Mutation chance", &g_mutationChance },
+                    { "Mutation count (min)", &g_minMutationCount },
+                    { "Mutation count (max)", &g_maxMutationCount }
                 }
             };
         }
@@ -39,34 +41,15 @@ namespace {
                 return;
             g_timeSinceLastMutation -= g_timeBetweenMutations;
 
-            for (const auto & [typeEntity, genome, get, size, forEachEntity] : kengine::entities.with<meta::GenomeComponent, kengine::meta::Get, kengine::meta::Size, kengine::meta::ForEachEntity>()) {
+            for (const auto & [typeEntity, genome, forEachEntity] : kengine::entities.with<meta::GenomeComponent, kengine::meta::ForEachEntity>()) {
                 forEachEntity([&](kengine::Entity e) {
                     if (putils::rand(0.f, 1.f) > g_mutationChance)
                         return;
-
-                    const auto comp = (char*)get(e);
-                    if (genome.attributes.empty())
-                        mutate(comp, size.size);
-                    else
-                        mutateRandomAttribute(comp, typeEntity, genome);
+                    const auto mutationCount = putils::rand(g_minMutationCount, g_maxMutationCount);
+                    for (int i = 0; i < mutationCount; ++i)
+                        mutationHelper::mutate(e, typeEntity);
                 });
             }
-        }
-
-        static void mutateRandomAttribute(char * comp, const kengine::Entity & typeEntity, const meta::GenomeComponent & genome) noexcept {
-            const auto mutatedAttributeIndex = putils::rand<size_t>(0, genome.attributes.size());
-            const auto & mutatedAttribute = genome.attributes[mutatedAttributeIndex];
-
-            const auto attributeInfo = kengine::meta::attributeHelper::findAttribute(typeEntity, mutatedAttribute.name);
-            if (attributeInfo)
-                mutatedAttribute.mutate(comp + attributeInfo->offset, attributeInfo->size, mutate);
-        }
-
-        static void mutate(void * data, size_t bytes) noexcept {
-            const auto mutationIndex = putils::rand<size_t>(0, bytes * 8);
-
-            putils::BitFieldRef bitField(data);
-            bitField.toggle(mutationIndex);
         }
 	};
 }
